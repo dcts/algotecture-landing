5ee2374245f13af328d0d78713e794c9d27028e3
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-06-15 23:49:21 +0200

    add head banner

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 11, duration: 0, style: "mapbox://styles/mapbox/dark-v9" });
 };
 
 const initMapbox = () => {
@@ -13,7 +13,7 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/light-v9'
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
