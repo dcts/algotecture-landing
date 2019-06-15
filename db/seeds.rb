54e54834b82cc66d1d9c75203e7ae8659c7bc6aa
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-06-15 20:18:07 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# RESET ALL DATA
+Building.delete_all
+
+# mockup buildings
+Building.create!(
+  title: "Building A45",
+  description: "Old building used for rental and for businesses",
+  location: "Feldzeugmeisterstrasse 5, Berlin",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Barclay%27s_Bank_building%2C_Sutton_%28Surrey%29%2C_Greater_London_03.jpg/1200px-Barclay%27s_Bank_building%2C_Sutton_%28Surrey%29%2C_Greater_London_03.jpg",
+)
+Building.create!(
+  title: "Skyscraper GJF2",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo saepe laudantium vel animi dolore sit reprehenderit, minus aliquam, aspernatur est tempore laborum ipsum, incidunt dolor quaerat perferendis amet, enim similique?",
+  location: "Oberbaumstraße 8, 10997 Berlin",
+  image_url: "https://static.thousandwonders.net/Chrysler.Building.original.8181.jpg",
+)
+Building.create!(
+  title: "Modern Appartment Space",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita tempore deserunt nam vitae totam reprehenderit quidem suscipit eveniet omnis praesentium consequatur, consequuntur facere earum error, doloribus delectus sequi quas sit.",
+  location: "Holzmarktstraße 25, 10243 Berlin",
+  image_url: "http://fytogreen.com.au/wp-content/uploads/2017/02/Medibank-Feb16_36.jpg",
+)
+Building.create!(
+  title: "Modern Factory",
+  description: "Used for ...",
+  location: "Ritterstraße 26, 10969 Berlin",
+  image_url: "https://perryroberts.files.wordpress.com/2012/09/1-minnaertgebouw-utrecht-nl1.jpg",
+)
